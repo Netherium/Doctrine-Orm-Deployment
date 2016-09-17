cc84d92158d039653faa59b937d76df907dc6dc9
gerpis <gerpis@gmail.com>
2016-09-17 14:41:28 +0300

    Initial Commit

--- src/MyBundle/Entity/Product.php
+++ src/MyBundle/Entity/Product.php
@@ -32,28 +32,4 @@ class Product {
     {
         return $this->id;
     }
-
-    /**
-     * Set name
-     *
-     * @param string $name
-     *
-     * @return Product
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    
-        return $this;
-    }
-
-    /**
-     * Get name
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
 }
